840029197c392de612f32618914caad3fef4cc5f
guaka <guaka@guaka.org>
2026-07-27 10:39:56 +0200

    feat: add current IP context for administrators

--- modules/core/tests/server/api-routes.unit.tests.js
+++ modules/core/tests/server/api-routes.unit.tests.js
@@ -513,6 +513,7 @@ describe('API route registrations', () => {
       [
         'changeRole',
         'getUser',
+        'listUsersByLastIpAddress',
         'listUsersByRole',
         'searchUsers',
         'usernameToUserId',
@@ -572,6 +573,10 @@ describe('API route registrations', () => {
       auditLog.record,
       users.listUsersByRole,
     ]);
+    assertHandlers(
+      routeByPath(routes, '/api/admin/users/by-last-ip-address').post,
+      [auditLog.record, users.listUsersByLastIpAddress],
+    );
     assertHandlers(routeByPath(routes, '/api/admin/user').post, [
       auditLog.record,
       users.getUser,
